d75e855ab2e31206618d390a8af870ea276cb256
yashpatel5400 <ypatel@princeton.edu>
2017-09-03 01:09:38 -0400

    Got refactor to compile

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,13 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+/* global artifacts */
+/* eslint-disable prefer-reflect */
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+const AO        = artifacts.require('AO.sol');
+const Balances  = artifacts.require('Balances.sol');
+const Backdoor  = artifacts.require('Backdoor.sol');
+const RewardDAO = artifacts.require('RewardDAO.sol');
+
+module.exports = async (deployer) => {
+    deployer.deploy(Backdoor);
+    await deployer.deploy(AO);
+    deployer.deploy(RewardDAO, AO.address);
 };
