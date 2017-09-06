0039f5d265cebd4acc9db456597c3b71c2968171
yashpatel5400 <ypatel@princeton.edu>
2017-09-06 00:39:16 -0400

    Reverted change

--- contracts/interfaces/IRewardDAO.sol
+++ contracts/interfaces/IRewardDAO.sol
@@ -1,11 +1,9 @@
 pragma solidity ^0.4.15;
 
 contract IRewardDAO {
-    // function getEthBalance() public returns (uint);
-
     function onDeposit(uint _amount) public returns (bool);
-
-    function claim() public payable;
+    function getEthBalance() public returns (uint);
+    function claim() public;
     function deposit(address _token, uint _amount) public payable;
-    function withdraw() public payable;
+    function withdraw() public;
 }
